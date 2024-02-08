8f5c10afb3e8979bc9150a643da1823b852b9522
Maxim babayev <mxm.bbv.05@gmail.com>
2024-02-08 14:29:05 +0600

    v0.1
    - Updated finance-insurance.php

--- config/finance-insurance.php
+++ config/finance-insurance.php
@@ -3,5 +3,5 @@
 return [
     'uri' => env('FREEDOM_INSURANCE_API_URI', 'https://apiv4.sandbox.kz'),
 
-    'headers' => json_decode(env('FREEDOM_INSURANCE_HEADERS', '{}')),
+    'headers' => json_decode(env('FREEDOM_INSURANCE_HEADERS', '{}'), true),
 ];
